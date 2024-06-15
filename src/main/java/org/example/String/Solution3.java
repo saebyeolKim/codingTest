1d9f8de4ac5f8514682de3b2a16f0e7367cb05f7
saebyeolKim <toquf2611@naver.com>
2024-06-15 15:07:23 +0900

    add: 1.문장 속 단어(indexOf(), substring())

---
+++ src/main/java/org/example/String/Solution3.java
@@ -0,0 +1,56 @@
+package org.example.String;
+
+import java.util.*;
+
+public class Solution3 {
+
+    public String solution(String str) {
+        String answer = "";
+        String[] arr = str.split(" ");
+        int n = 0;
+        for (String a : arr) {
+            if (a.length() > n) {
+                answer = a;
+                n = a.length();
+            }
+        }
+        return answer;
+    }
+
+    public String correct(String str) {
+        String answer = "";
+        int m = Integer.MIN_VALUE;
+        String[] s = str.split(" ");
+        for (String x : s) {
+            int len = x.length();
+            if (len > m) {
+                m = len;
+                answer = x;
+            }
+        }
+        return answer;
+    }
+
+    public String correct2(String str) {
+        String answer = "";
+        int m = Integer.MIN_VALUE, pos;
+        while ((pos = str.indexOf(" ")) != -1) {
+            String tmp = str.substring(0, pos);
+            int len = tmp.length();
+            if (len > m) {
+                m = len;
+                answer = tmp;
+            }
+            str = str.substring(pos + 1);
+        }
+        if (str.length() > m) answer = str;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input1 = in.nextLine();
+        Solution3 s = new Solution3();
+        System.out.println(s.correct(input1));
+    }
+}
