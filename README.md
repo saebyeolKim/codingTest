4dec8a6dfa6f13103830335aca6c39a0e6a889d0
saebyeolKim <toquf2611@naver.com>
2024-06-16 11:13:49 +0900

    add: 6.회문문자열 - equalsIgnoreCase String 값 비교시 대소문자 구분안하고 비교

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 빌드하는 법
 - ./gradlew build  
 - java -jar ./build/libs/codingTest-1.0-SNAPSHOT.jar
+- 기본 solution format
+
+public String solution(String str) {
+  String answer = "";
+  return answer;
+}
+
+public static void main(String[] args) {
+  Solution7 s = new Solution7();
+  Scanner in = new Scanner(System.in);
+  String input = in.next();
+  System.out.println(s.solution(input));
+}
---
+++ src/main/java/org/example/String/Solution7.java
@@ -0,0 +1,46 @@
+package org.example.String;
+
+import java.util.Scanner;
+
+public class Solution7 {
+
+    public String solution(String str) {
+        String answer = "YES";
+        char[] c = str.toLowerCase().toCharArray();
+        int lt = 0;
+        int rt = str.length() - 1;
+        while(lt < rt) {
+            if (c[lt] != c[rt]) return "NO";
+            else {
+                lt++;
+                rt--;
+            }
+        }
+        return answer;
+    }
+
+    public String teacher(String str) {
+        String answer = "YES";
+        str = str.toLowerCase();
+        int len = str.length();
+        for (int i = 0; i < len/2; i++) {
+            if (str.charAt(i) != str.charAt(len - i - 1)) return "NO";
+        }
+        return answer;
+    }
+
+    public String teacher2(String str) {
+        String answer = "YES";
+        String tmp = new StringBuilder(str).reverse().toString();
+        //equalsIgnoreCase 대소문자 구분안함
+        if (!str.equalsIgnoreCase(tmp)) return "NO";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution7 s = new Solution7();
+        Scanner in = new Scanner(System.in);
+        String input = in.next();
+        System.out.println(s.teacher2(input));
+    }
+}
