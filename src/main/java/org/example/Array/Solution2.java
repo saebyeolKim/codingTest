ff7c7211724227cc087777c21b5bd6a330df86ee
saebyeolKim <toquf2611@naver.com>
2024-06-18 21:09:21 +0900

    add: 2.보이는 학생

---
+++ src/main/java/org/example/Array/Solution2.java
@@ -0,0 +1,37 @@
+package org.example.Array;
+import java.util.*;
+public class Solution2 {
+    public int solution(int n, int[] arr) {
+        int answer = 1;
+        int max = arr[0];
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] < arr[i + 1] && max < arr[i + 1]) {
+                answer++;
+                max = arr[i + 1];
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(s.solution(n,arr));
+    }
+
+    public int teacher(int n, int[] arr) {
+        int answer = 1, max = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                answer++;
+                max = arr[i];
+            }
+        }
+        return answer;
+    }
+}
