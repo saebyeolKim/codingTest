f4f5d556c18a0cf71f99feb324204158ba1d31d1
Saebyeol <94090893+saebyeolKim@users.noreply.github.com>
2024-06-19 10:19:53 +0900

    add: 4.피보나치 수열

---
+++ src/main/java/org/example/Array/Solution4.java
@@ -0,0 +1,22 @@
+package org.example.Array;
+import java.util.*;
+public class Solution4 {
+    public int[] solution(int n) {
+        int[] answer = new int[n];
+        answer[0] = 1;
+        answer[1] = 1;
+        for (int i = 2; i < n; i++) {
+            answer[i] = answer[i - 1] + answer[i - 2];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution4 s = new Solution4();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int x : s.solution(n)) {
+            System.out.print(x + " ");
+        }
+    }
+}
