795b3c038087bc82e0f0bcc56017d80611560536
saebyeolKim <toquf2611@naver.com>
2024-06-19 21:45:35 +0900

    add: 4.피보나치 수열 - 강의

--- src/main/java/org/example/Array/Solution4.java
+++ src/main/java/org/example/Array/Solution4.java
@@ -11,6 +11,27 @@ public class Solution4 {
         return answer;
     }
 
+    public int[] teacher(int n) {
+        int[] answer = new int[n];
+        answer[0] = 1;
+        answer[1] = 1;
+        for (int i = 2; i < n; i++) {
+            answer[i] = answer[i - 1] + answer[i - 2];
+        }
+        return answer;
+    }
+
+    public void teacher2(int n) {
+        int a = 1, b = 1, c;
+        System.out.print(a + " " + b + " ");
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+
     public static void main(String[] args) {
         Solution4 s = new Solution4();
         Scanner in = new Scanner(System.in);
