a1e9fb1df109d26c38e37e05535388d3021b1549
saebyeolKim <toquf2611@naver.com>
2024-06-20 19:10:27 +0900

    add: 6.뒤집은 소수

---
+++ src/main/java/org/example/Array/Solution6.java
@@ -0,0 +1,68 @@
+package org.example.Array;
+import java.util.*;
+public class Solution6 {
+
+    public String solution(int n, int[] arr) {
+        String answer = "";
+        for (int i = 0; i < n; i++) {
+            StringBuilder sd = new StringBuilder(String.valueOf(arr[i])).reverse();
+            int num = Integer.parseInt(String.valueOf(sd));
+            for (int j = 2; j <= num; j++) {
+                if (num % j == 0) {
+                    if (num == j) answer += num + " ";
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+
+
+    public static void main(String[] args) {
+        Solution6 s = new Solution6();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(s.solution(n, arr));
+    }
+
+    public boolean isPrime(int num) {
+        if (num == 1) return false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public ArrayList<Integer> teacher(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int tmp = arr[i];
+            int res = 0;
+            while (tmp > 0) {
+                int t = tmp % 10;
+                res = res * 10 + t;
+                tmp /= 10;
+            }
+            if (isPrime(res)) answer.add(res);
+        }
+        return answer;
+    }
+
+    public static void teacherMain(String[] args) {
+        Solution6 s = new Solution6();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int x : s.teacher(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
