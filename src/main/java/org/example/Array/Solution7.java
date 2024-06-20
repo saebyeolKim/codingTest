99a042e994b0618790da7280c0df9954e7b7a095
saebyeolKim <toquf2611@naver.com>
2024-06-20 20:24:07 +0900

    add: 7.점수계산

---
+++ src/main/java/org/example/Array/Solution7.java
@@ -0,0 +1,38 @@
+package org.example.Array;
+import java.util.*;
+public class Solution7 {
+    public int solution(int n, int[] arr) {
+        int answer = 0;
+        int cur = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) cur = 0;
+            else cur++;
+            answer += cur;
+        }
+        return answer;
+    }
+
+    public int teacher(int n, int[] arr) {
+        int answer = 0, cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1) {
+                cnt++;
+                answer += cnt;
+            } else {
+                cnt = 0;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution7 s = new Solution7();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(s.solution(n, arr));
+    }
+}
