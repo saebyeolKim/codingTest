2b57ad0f76625fc463720f761488126cf78949f4
saebyeolKim <toquf2611@naver.com>
2024-06-21 11:43:27 +0900

    add: 9.격자판 최대합

---
+++ src/main/java/org/example/Array/Solution9.java
@@ -0,0 +1,88 @@
+package org.example.Array;
+import java.util.*;
+public class Solution9 {
+
+    public int rowMax(int n, int[][] arr) {
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += arr[i][j];
+            }
+            if (sum > max) max = sum;
+        }
+        return max;
+    }
+
+    public int columnMax(int n, int[][] arr) {
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += arr[j][i];
+            }
+            if (sum > max) max = sum;
+        }
+        return max;
+    }
+
+    public int downMax(int n, int[][] arr) {
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max += arr[i][i];
+        }
+        return max;
+    }
+
+    public int upMax(int n, int[][] arr) {
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max += arr[i][n - i - 1];
+        }
+        return max;
+    }
+
+    public int solution(int n, int[][]arr) {
+        int answer = Math.max(rowMax(n, arr), columnMax(n, arr));
+        answer = Math.max(answer, downMax(n, arr));
+        answer = Math.max(answer, upMax(n, arr));
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution9 s = new Solution9();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+        System.out.println(s.teacher(n, arr));
+    }
+
+    public int teacher(int n, int[][]arr) {
+        int answer = Integer.MIN_VALUE;
+        int sum1, sum2;
+        //행과 열의 최대값
+        for (int i = 0; i < n; i++) {
+            sum1 = sum2 = 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+        //대각선 최대값
+        sum1 = sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[i][n - i - 1];
+        }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+        return answer;
+    }
+}
