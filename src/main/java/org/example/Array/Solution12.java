60194794dfa9b3758e17c0d359be85335bcd8a18
saebyeolKim <toquf2611@naver.com>
2024-06-22 14:12:42 +0900

    add: 12.멘토링 (다시)

---
+++ src/main/java/org/example/Array/Solution12.java
@@ -0,0 +1,65 @@
+package org.example.Array;
+import java.util.*;
+public class Solution12 {
+    public int solution(int n, int m, int[][]arr) {
+        int answer = 0;
+        //n:학생수(4), m:수학테스트 차수(3),
+        for (int i = 0; i < n; i++) {
+            int myRank = i;
+            int mt = n - i - 1;
+            for (int j = 0; j < m; j++) {
+                //타겟 arr[0][2]
+                //arr[0,0] arr[0,1] arr[0,2] arr[0,3]
+                //arr[1,0] arr[1,1] arr[1,2] arr[1,3]
+                //arr[2,0] arr[2,1] arr[2,2] arr[2,3]
+                //첫번째 돌았을 때 1등해서 3명을 가르칠 수 있음 (돌면서 몇명 가르칠 수 있는지 확인)
+                //두번째 돌았을 때 2등해서 2명을 가르칠 수 있음 (이전 등수 보다 낮으면 --)
+                //마지막 돌았을 때 1등해서 2명을 가르칠 수 있음 (이전 등수 보다 높으면 break;)
+                for (int k = 0; k < n; k++) {
+                    if (myRank < k && arr[j][i] == arr[j][k]) {
+                        mt--;
+                        myRank = k;
+                    }
+                    if (myRank > k) break;
+                }
+                System.out.println("arr[j][i] = " + arr[j][i] + " mt = " + mt);
+            }
+        }
+        return answer;
+    }
+
+    public int teacher (int n, int m, int[][]arr) {
+        int answer = 0;
+        //i와 j가 멘토 멘티가 될 수 있는가
+        //1번은 1,2,3,4 와 멘토멘티 => 멘토멘티가 될 수 있는 경우의 수 = n * n
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int cnt = 0;
+                for (int k = 0; k < m; k++) {
+                    int pi = 0, pj = 0;
+                    for (int s = 0; s < n; s++) {
+                        if (arr[k][s] == i) pi = s;
+                        if (arr[k][s] == j) pj = s;
+                    }
+                    if (pi < pj) cnt++;
+                }
+                if (cnt == m) answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution12 s = new Solution12();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+        System.out.println(s.solution(n, m, arr));
+    }
+}
