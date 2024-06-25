00a1ca7c7ca6563b6705156ed89a5fc735d7dcac
saebyeolKim <toquf2611@naver.com>
2024-06-25 21:18:15 +0900

    add: 5.연속된 자연수의 합(two pointers)

---
+++ src/main/java/org/example/TwoPointersSlidingWindow/Solution5.java
@@ -0,0 +1,43 @@
+package org.example.TwoPointersSlidingWindow;
+import java.util.*;
+public class Solution5 {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += j;
+                if (sum == n) {
+                    answer++;
+                    break;
+                } else if (sum > n) break;
+            }
+        }
+        return answer;
+    }
+
+    //연속된 자연수는 n / 2 + 1 까지만 돌면 된다.
+    //two pointers algorithms
+    public int teacher(int n) {
+        int answer = 0, sum = 0, lt = 0;
+        int m =  n/ 2 + 1;
+        int[] arr = new int[m];
+        for (int i = 0; i < m; i++) arr[i] = i + 1;
+        for (int rt = 0; rt < m; rt++) {
+            sum += arr[rt];
+            if (sum == n) answer++;
+            while (sum >= n) {
+                sum -= arr[lt++];
+                if (sum == n) answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution5 s = new Solution5();
+        Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
+        System.out.println(s.solution(input));
+    }
+}
