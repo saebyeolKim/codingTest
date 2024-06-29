974e4b392278e5b177f40e98e483e2c40e7b7b14
saebyeolKim <toquf2611@naver.com>
2024-06-29 13:33:45 +0900

    add: 1.올바른 괄호

---
+++ src/main/java/org/example/StackQueue/Solution1.java
@@ -0,0 +1,35 @@
+package org.example.StackQueue;
+import java.util.*;
+public class Solution1 {
+    public String solution(String str) {
+        if (str.charAt(0) == ')') return "NO";
+        Stack<Character> st = new Stack<>();
+        st.push(str.charAt(0));
+        for (int i = 1; i < str.length(); i++) {
+            if (!st.isEmpty() && st.peek() == '(' && str.charAt(i) == ')') st.pop();
+            else st.push(str.charAt(i));
+        }
+        return st.size() > 0 ? "NO" : "YES";
+    }
+
+    public String teacher(String str) {
+        String answer = "YES";
+        Stack<Character> stack = new Stack<>();
+        for (char x : str.toCharArray()) {
+            if (x == '(') stack.push(x);
+            else {
+                if (stack.isEmpty()) return "NO";
+                stack.pop();
+            }
+        }
+        if (!stack.isEmpty()) return "NO";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution1 s = new Solution1();
+        Scanner in = new Scanner(System.in);
+        String input = in.next();
+        System.out.println(s.solution(input));
+    }
+}
