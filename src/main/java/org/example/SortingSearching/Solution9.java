6b5412ba6aab18097d1126e36ae4c64330b37462
saebyeolKim <toquf2611@naver.com>
2024-07-07 14:54:17 +0900

    add: 9.뮤직비디오(결정알고리즘) - 다시 도전

---
+++ src/main/java/org/example/SortingSearching/Solution9.java
@@ -0,0 +1,44 @@
+package org.example.SortingSearching;
+
+import java.util.*;
+
+public class Solution9 {
+
+    public int count(int[] arr, int capacity) {
+        int cnt = 1, sum = 0;
+        for (int x : arr) {
+            if (sum + x > capacity) {
+                cnt++;
+                sum = x;
+            } else sum += x;
+        }
+        return cnt;
+    }
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) <= m) {
+                answer = mid;
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution9 s = new Solution9();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(s.solution(n, m, arr));
+    }
+}
