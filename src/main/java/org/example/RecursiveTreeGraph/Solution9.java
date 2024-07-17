397c5236c800dd9b4928a2bbb06f399d78097139
saebyeolKim <toquf2611@naver.com>
2024-07-17 21:40:10 +0900

    add: 9,10 Tree 말단노드까지의 가장 짧은 경로(DFS, BFS)

---
+++ src/main/java/org/example/RecursiveTreeGraph/Solution9.java
@@ -0,0 +1,41 @@
+package org.example.RecursiveTreeGraph;
+import java.util.*;
+
+public class Solution9 {
+    Node root;
+    public int BFS(Node root) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.add(root);
+        int L = 0;
+        while(!Q.isEmpty()) {
+            int len = Q.size();
+            for (int i = 0; i < len; i++) {
+                Node cur = Q.poll();
+                if (cur.lt == null && cur.rt == null) return L;
+                if (cur.lt != null) Q.add(cur.lt);
+                if (cur.rt != null) Q.add(cur.rt);
+            }
+            L++;
+        }
+        return 0;
+    }
+
+    public int DFS(int L, Node root) {
+        if (root.lt == null && root.rt == null) return L;
+        else return Math.min(DFS(L + 1, root.lt), DFS(L + 1, root.rt));
+    }
+
+    public static void main(String[] args) {
+        Solution9 tree = new Solution9();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
+        tree.root.rt.rt.lt = new Node(8);
+        tree.root.rt.rt.rt = new Node(9);
+        System.out.println(tree.BFS(tree.root));
+    }
+}
