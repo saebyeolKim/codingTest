9c0b639be4283875cd8e1dff1f27bfbeac615055
saebyeolKim <toquf2611@naver.com>
2025-01-02 21:46:36 +0900

    review: 7. 회문 문자열

--- src/main/java/org/example/String/Solution7.java
+++ src/main/java/org/example/String/Solution7.java
@@ -41,6 +41,22 @@ public class Solution7 {
         Solution7 s = new Solution7();
         Scanner in = new Scanner(System.in);
         String input = in.next();
-        System.out.println(s.teacher2(input));
+        System.out.println(s.review1(input));
+    }
+
+    public String review1(String str) {
+        str = str.toLowerCase();
+        char[] arr = str.toCharArray();
+        int lt = 0;
+        int rt = str.length() - 1;
+        while (lt < rt) {
+            if (arr[lt] == arr[rt]) {
+                lt++;
+                rt--;
+            } else {
+                return "NO";
+            }
+        }
+        return "YES";
     }
 }
