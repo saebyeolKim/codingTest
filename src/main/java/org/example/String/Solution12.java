f301f57bcbc5a238017de478b2974c7eba24721f
saebyeolKim <toquf2611@naver.com>
2025-01-10 14:40:29 +0900

    review: 12. 암호

--- src/main/java/org/example/String/Solution12.java
+++ src/main/java/org/example/String/Solution12.java
@@ -24,7 +24,7 @@ public class Solution12 {
         Scanner in = new Scanner(System.in);
         int input1 = in.nextInt();
         String input2 = in.next();
-        System.out.print(s.solution(input1, input2));
+        System.out.print(s.review1(input1, input2));
     }
 
     public String teacher(int n, String s) {
@@ -34,6 +34,17 @@ public class Solution12 {
             int num = Integer.parseInt(tmp, 2); //2진수를 10진수로
             answer += (char) num;
             s = s.substring(7);
+        }
+        return answer;
+    }
+
+    public String review1(int n, String str) {
+        str = str.replace("#", "1").replace("*", "0");
+        String answer = "" + (char)Integer.parseInt(str.substring(0, 7), 2);
+        for (int i = 7; i < str.length(); i++) {
+            if (i % 7 == 0) {
+                answer += "" + (char)Integer.parseInt(str.substring(i, i + 7), 2);
+            }
         }
         return answer;
     }
