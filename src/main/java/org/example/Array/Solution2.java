43731c70cf4bc867f5e0988626560c54760f3964
saebyeolKim <toquf2611@naver.com>
2025-01-15 14:01:46 +0900

    review: 2. 보이는 학생

--- src/main/java/org/example/Array/Solution2.java
+++ src/main/java/org/example/Array/Solution2.java
@@ -21,13 +21,25 @@ public class Solution2 {
         for (int i = 0; i < n; i++) {
             arr[i] = in.nextInt();
         }
-        System.out.println(s.solution(n,arr));
+        System.out.println(s.review1(arr));
     }
 
     public int teacher(int n, int[] arr) {
         int answer = 1, max = arr[0];
         for (int i = 0; i < n; i++) {
             if (arr[i] > max) {
+                answer++;
+                max = arr[i];
+            }
+        }
+        return answer;
+    }
+
+    public int review1(int[] arr){
+        int answer = 1;
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (max < arr[i]) {
                 answer++;
                 max = arr[i];
             }
