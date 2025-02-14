663818f943354e63359d1883bb1dba990ab8c3c0
saebyeolKim <toquf2611@naver.com>
2025-02-14 22:01:40 +0900

    review: 4. 후위식 연산(postfix) - challenge

--- src/main/java/org/example/StackQueue/Solution4.java
+++ src/main/java/org/example/StackQueue/Solution4.java
@@ -21,6 +21,22 @@ public class Solution4 {
         Solution4 s = new Solution4();
         Scanner in = new Scanner(System.in);
         String input = in.next();
-        System.out.println(s.solution(input));
+        System.out.println(s.review1(input));
+    }
+
+    public int review1(String s) {
+        Stack<Integer> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) st.add(c- 48);
+            else {
+                int rt = st.pop();
+                int lt = st.pop();
+                if (c == '+') st.add(lt + rt);
+                else if (c == '-') st.add(lt - rt);
+                else if (c == '*') st.add(lt * rt);
+                else if (c == '/') st.add(lt / rt);
+            }
+        }
+        return st.get(0);
     }
 }
