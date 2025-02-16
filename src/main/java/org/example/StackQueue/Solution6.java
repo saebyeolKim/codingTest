e2384190ab1b36bf7960e5c4373e49099129f42d
saebyeolKim <toquf2611@naver.com>
2025-02-16 23:18:07 +0900

    review: 6. 공주 구하기 - challenge

--- src/main/java/org/example/StackQueue/Solution6.java
+++ src/main/java/org/example/StackQueue/Solution6.java
@@ -23,6 +23,19 @@ public class Solution6 {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int k = in.nextInt();
-        System.out.println(s.solution(n, k));
+        System.out.println(s.review1(n, k));
+    }
+
+    public int review1(int n, int k) {
+        int answer = 0;
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) q.offer(i);
+        while (!q.isEmpty()) {
+            for (int i = 1; i < k; i++) q.offer(q.poll());
+            q.poll();
+            if (q.size() == 1) return q.poll();
+
+        }
+        return answer;
     }
 }
