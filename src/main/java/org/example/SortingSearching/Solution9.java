f941afe3e9c65b2a7802ad36ea267e9b7be0b1dc
saebyeolKim <toquf2611@naver.com>
2025-03-06 22:13:31 +0900

    review: 9. 뮤직비디오(결정알고리즘) - challenge

--- src/main/java/org/example/SortingSearching/Solution9.java
+++ src/main/java/org/example/SortingSearching/Solution9.java
@@ -39,13 +39,43 @@ public class Solution9 {
         for (int i = 0; i < n; i++) {
             arr[i] = in.nextInt();
         }
-        System.out.println(s.solution(n, m, arr));
+        System.out.println(s.review1(n, m, arr));
     }
 
     public int review1(int n, int m, int[] arr) {
+        //결정알고리즘
+        //순서를 유지하면서, 3장에 담아낼 수 있는 1장의 최소 용량
+        //lt -------------------- rt
+        //lt 부터 rt 사이의 확실히 답이 있을 때만 결정알고리즘을 사용해야 함
+        //절반을 잘라서 절반을 자른 값이 이 문제의 답으로 가능한 숫자가 될 수 있는가?
+        //arr 의 모든 숫자를 더해서 최소값(lt), 최대값(rt)으로 풀어서 절반을 나눠, 점차 줄이거나 늘려야 함
+        //lt = 9, rt = 45
         int answer = 0;
         Arrays.sort(arr);
-//        for (int i = 0; )
+        int lt = arr[arr.length - 1];
+        int rt = 0;
+        for (int a : arr) rt += a;
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (countDVDs(arr, mid) <= m) { //DVD 개수가 M 이하라면 가능
+                answer = mid;
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
+        }
         return answer;
+    }
+
+    public int countDVDs(int[] arr, int capacity) {
+        int count = 1; //최소 DVD 가 한장은 필요하기 때문
+        int sum = 0;
+        for (int a : arr) {
+            if (sum + a > capacity) { //정해진 용량을 초과하면
+                count++; //DVD 한장 더 필요
+                sum = a;
+            } else sum += a;
+        }
+        return count;
     }
 }
