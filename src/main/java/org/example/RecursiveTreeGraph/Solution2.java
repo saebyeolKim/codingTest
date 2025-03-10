afd2e8aa079c62a2bb2953bbb2e2cb28f80bc989
saebyeolKim <toquf2611@naver.com>
2025-03-10 21:19:25 +0900

    review: 2. 이진수 출력(재귀)

--- src/main/java/org/example/RecursiveTreeGraph/Solution2.java
+++ src/main/java/org/example/RecursiveTreeGraph/Solution2.java
@@ -10,6 +10,12 @@ public class Solution2 {
     }
     public static void main(String[] args) {
         Solution2 T = new Solution2();
-        T.DFS(11);
+        T.review1(11);
+    }
+
+    public void review1(int n) {
+        if (n == 0) return;
+        review1(n / 2);
+        System.out.print(n % 2);
     }
 }
