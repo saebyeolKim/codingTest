9389e3da93b7305c37fe506da6660886501773f8
saebyeolKim <toquf2611@naver.com>
2025-03-11 20:18:32 +0900

    review: 3. 팩토리얼

--- src/main/java/org/example/RecursiveTreeGraph/Solution3.java
+++ src/main/java/org/example/RecursiveTreeGraph/Solution3.java
@@ -7,6 +7,17 @@ public class Solution3 {
     }
     public static void main(String[] args) {
         Solution3 T = new Solution3();
-        System.out.println(T.DFS(5));
+        System.out.println(T.review1(5));
+    }
+
+    public int review1(int n) {
+        if (n == 1) return 1;
+        else return n * review1(n - 1);
+        //D(5) 5 * D(4)
+        //D(4)     4 * D(3)
+        //D(3)         3 * D(2)
+        //D(2)             2 * D(1)
+        //D(1)                 1    위로 올라간다.
+        //1 * 2 * 3 * 4 * 5 백트래킹으로 이렇게 실행된다.
     }
 }
