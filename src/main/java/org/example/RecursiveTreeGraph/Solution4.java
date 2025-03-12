7b1822635f74fc48bea65a479d70d916455c1a7a
saebyeolKim <toquf2611@naver.com>
2025-03-12 23:45:33 +0900

    review: 4.피보나치 수열

--- src/main/java/org/example/RecursiveTreeGraph/Solution4.java
+++ src/main/java/org/example/RecursiveTreeGraph/Solution4.java
@@ -11,11 +11,54 @@ public class Solution4 {
             return fibo[n] = DFS(n - 2) + DFS(n - 1);
         }
     }
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         Solution4 T = new Solution4();
         int n = 45;
         fibo = new int[n + 1];
         T.DFS(n);
         for (int i = 1; i <= n; i++) System.out.println(fibo[i] + " ");
+    }
+
+    public static void mainReview1(String[] args) {
+        Solution4 T = new Solution4();
+        int n = 7;
+        System.out.println(T.review1(n));
+    }
+
+    //n번째 항을 구함
+    public int review1(int n) {
+        if (n == 1) return 1;
+        else if (n == 2) return 1;
+        else return review1(n - 2) + review1(n - 1);
+        //D(5) -> D(3) + D(4) = 2 + 3 = 5
+        //D(3) -> D(1) + D(2) = 1 + 1 = 2
+        //D(4) -> D(2) + D(3) = 1 + 2 = 3
+    }
+    static int[] fb;
+    public static void main(String[] args) {
+        Solution4 T = new Solution4();
+        int n = 45;
+        //이렇게 짤 수 있지만, 시간이 오래 걸림
+//        for (int i = 1; i <= n; i++) System.out.print(T.review1(i) + " ");
+        //이렇게 짜야함
+        fb = new int[n + 1]; //0번 인덱스 사용안함
+        T.review1_2(n);
+        for (int i = 1; i <= n; i++) System.out.print(fb[i] + " ");
+    }
+
+    //이것도 좀 오래걸림
+    public int review1_1(int n) {
+        if (n == 1) return fb[n] = 1;
+        else if (n == 2) return fb[n] = 1;
+        else return fb[n] = review1_1(n - 2) + review1_1(n - 1);
+    }
+
+    //이미 구해 놓은 값이 있는 지 확인해서 있으면 넘어가기
+    //금방 나옴!
+    public int review1_2(int n) {
+        if (fb[n] != 0) return fb[n];
+        if (n == 1) return fb[n] = 1;
+        else if (n == 2) return fb[n] = 1;
+        else return fb[n] = review1_2(n - 2) + review1_2(n - 1);
     }
 }
