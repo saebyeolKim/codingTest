37b144e76459ec303dcaa765dc2cb9da86ecd4fa
saebyeolKim <toquf2611@naver.com>
2025-04-13 21:59:59 +0900

    review: 1. 합이 같은 부분집합

--- src/main/java/org/example/DFSBFS/Solution1.java
+++ src/main/java/org/example/DFSBFS/Solution1.java
@@ -28,8 +28,22 @@ public class Solution1 {
             arr[i] = sc.nextInt();
             total += arr[i];
         }
-        s.DFS(0, 0, arr);
+        s.reviewDFS(0, 0, arr);
         System.out.println(answer);
+    }
+
+    public void reviewDFS(int L, int sum, int[] arr) { //sum : 부분집합의 합
+        if (flag) return;
+        if (sum > total / 2) return;
+        if (L == n) {
+            if (total - sum == sum) { //부분집합의 합이 total 과 같다는게 판명나면 flag 를 세워서 뒤에 있는 것들이 안돌게 한다.
+                answer = "YES";
+                flag = true;
+            }
+        } else {
+            reviewDFS(L + 1, sum + arr[L], arr);
+            reviewDFS(L + 1, sum, arr);
+        }
     }
 }
 
