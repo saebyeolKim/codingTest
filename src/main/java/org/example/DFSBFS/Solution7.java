57461319c1b3657c737568304b1c6875b901cc09
saebyeolKim <toquf2611@naver.com>
2025-04-23 16:56:50 +0900

    add: 7. 조합수(메모이제이션)

---
+++ src/main/java/org/example/DFSBFS/Solution7.java
@@ -0,0 +1,22 @@
+package org.example.DFSBFS;
+
+import java.util.Scanner;
+
+public class Solution7 {
+
+    int[][] dy = new int[35][35];
+    public static void main(String[] args) {
+        Solution7 s = new Solution7();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        s.DFS(n , r);
+        System.out.println(s.DFS(n , r));
+    }
+
+    public int DFS(int n, int r) {
+        if (dy[n][r] > 0) return dy[n][r];
+        if (r == 0 || n == r ) return 1;
+        else return dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r);
+    }
+}
